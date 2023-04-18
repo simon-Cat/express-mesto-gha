c454a750179250f1b10a35926bfc0f771e3f3920
GregoMur <GregoMur@yandex.ru>
2023-04-18 18:19:49 +0300

    feature: add routes and controllers

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const router = require('./routes');
 
 const PORT = 3000;
 const app = express();
+
+app.use('/', router);
 
 mongoose.connect("mongodb://localhost:27017/mestodb").then(() => {
   console.log("Connected!");
